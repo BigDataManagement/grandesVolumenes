e2fe1656bd179ba6c607d196b841972b0a84c0d1
Edwin Montoya Jaramillo <emonto15@eafit.edu.co>
2019-04-11 18:07:46 -0500

    trigger for updating customer emails in the OLAP

--- Reto4/triggers.sql
+++ Reto4/triggers.sql
@@ -1,12 +1,7 @@
-
 delimiter |
 CREATE TRIGGER sakila.customer_email BEFORE UPDATE ON sakila.customer
   FOR EACH ROW BEGIN
-    UPDATE sakila_olap.customer_dimension SET email = CONCAT(email,',', NEW.email) WHERE sakila_olap.customer_dimension.customer_id = NEW.customer_id;
+  SET @email=NEW.email;
+  UPDATE sakila_olap.customer_dimension SET email =  IF(email NOT LIKE CONCAT('%',@email,'%'), CONCAT(email,',', NEW.email), email) WHERE sakila_olap.customer_dimension.customer_id = NEW.customer_id;
   END
   |
-CREATE TRIGGER sakila.cantidad_rentas_dia AFTER INSERT ON sakila.rental
-FOR EACH ROW BEGIN
-UPDATE sakila_olap.rental_fact SET cantidad_rentas_dia = cantidad_rentas_dia + 1 WHERE DATE_FORMAT(DATE(sakila_olap.rental_fact.time_id),'%Y%m%d') = DATE_FORMAT(DATE(NEW.rental_date),'%Y%m%d');
-END
-|
